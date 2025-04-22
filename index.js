6d0a2cb56f4c49e7549360b1309731fe3ad440f5
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-04-22 12:27:59 +0530

    Schema defined

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ main().then(() => {
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/test');
 }
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number
+})
