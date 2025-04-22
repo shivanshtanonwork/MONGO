ead5c5535a8a0309f6300ffce77d8269157d7f5c
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-04-22 12:13:08 +0530

    Setting up Mongo

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+main().then(() => {
+    console.log("connection successfull");
+
+})
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
